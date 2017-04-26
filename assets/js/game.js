af07769ec69554e4059e74adadc4bbb74ddd097c
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-04-26 11:53:36 -0400

    working underscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@
   var wins =0;
   var losses = 0;
 //number of guesses remaining  start with 15 -= 1
-  var maxGuesses = 15;
+  var maxGuesses = 10;
 //
 
   var words = ["bobafett", "hansolo", "luke", "rey", "leia", "skywalker", "jedi", "sith"];
@@ -11,46 +11,47 @@
 //array to store guesses
   var answerArray = [];
 //selected word from array
-  var lettersGuessed =[];
+  var lettersGuessed = [];
 
 //current word display as "_" inside html of browser
     for (var i = 0; i < selectWord.length; i++) {
       answerArray[i] = "_ ";
 // put in a string
         var newWord = answerArray.join(" ");
-// display selected word as "_" in the DOM
-        $("#currentWord").html(newWord);
+  $("#currentWord").html(newWord);
 }
 
 // press any key to guess letter for word
   document.onkeyup = function(event){
       var userGuess = event.key;
       console.log(userGuess);
-      maxGuesses --;
+
 
 // on event.key letter press replace _ with actual letter
-      // var letter = userGuess;
-      // if (letter.length > 0){
-      //   for (var i = 0; i < selectWord.length; i++) {
-      //     if(selectWord[i] === letter){
-      //       answerArray[i] = letter;
-      //
-      //     }
-      //   }
-      // };
-
-
-
-// if a letter is guessed and correct from the selectWord then replace "_" with letter
-//if letter is guessed and wrong add letter to lettersGuessed array subtract from maxGuesses
-
-
-//conditional statements for game logic
+      if (userGuess.length > 0){
+        for (var i = 0; i < selectWord.length; i++) {
+          if(selectWord[i] === userGuess){
+            answerArray[i] = userGuess;
+            // display selected word as "_" in the DOM
+            $("#currentWord").html(answerArray);
+            maxGuesses -=1;
+          }
+        }
+      }else if (userGuess.length > 0) {
+        for (var i = 0; i < selectWord.length; i++) {
+          if(selectWord[i].indexOf(userGuess === -1)){
+            lettersGuessed[i] = userGuess;
+            $('#lettersAlready').html(lettersGuessed.join(', '));
+            maxGuesses -=1;
+        }
+      }
 }
+insertHtml();
+}
+
+//if letter is guessed and wrong add letter to lettersGuessed array subtract from maxGuesses
 //create array of letters already guessed and not allow to replace in guesses
 // if you guess all the correct words pop up "YOU WIN!" add to WIN SCORE
-
-
 function insertHtml() {
   var html =
   "<p>Wins: " + wins + "</p>" +
@@ -59,5 +60,3 @@ function insertHtml() {
   $("#win").html(html);
 
 }
-
-insertHtml();
