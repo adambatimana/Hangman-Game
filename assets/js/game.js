779c454b2400074c2985fbbe1e907901fc81d8f6
Adam Batimana <adambatimana@gmail.com>
2017-04-26 10:14:22 -0400

    update

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,35 +16,39 @@
 //current word display as "_" inside html of browser
     for (var i = 0; i < selectWord.length; i++) {
       answerArray[i] = "_ ";
-    }
 // put in a string
-var newWord = answerArray.join(" ");
-$("#currentWord").html(newWord);
+        var newWord = answerArray.join(" ");
+// display selected word as "_" in the DOM
+        $("#currentWord").html(newWord);
+}
 
 // press any key to guess letter for word
   document.onkeyup = function(event){
-
       var userGuess = event.key;
-      var letter = $('word').userGuess;
-      if (letter.length > 0){
-        for (var i = 0; i < selectWord.length; i++) {
-          if(selectWord[i] === letter){
-            answerArray[i] = letter;
-          }
-        }
-      }
+      console.log(userGuess);
+      maxGuesses --;
+
+// on event.key letter press replace _ with actual letter
+      // var letter = userGuess;
+      // if (letter.length > 0){
+      //   for (var i = 0; i < selectWord.length; i++) {
+      //     if(selectWord[i] === letter){
+      //       answerArray[i] = letter;
+      //
+      //     }
+      //   }
+      // };
+
+
+
 // if a letter is guessed and correct from the selectWord then replace "_" with letter
 //if letter is guessed and wrong add letter to lettersGuessed array subtract from maxGuesses
-      maxGuesses -= 1;
+
 
 //conditional statements for game logic
-
-
-
-
 }
 //create array of letters already guessed and not allow to replace in guesses
-
+// if you guess all the correct words pop up "YOU WIN!" add to WIN SCORE
 
 
 function insertHtml() {
@@ -52,7 +56,7 @@ function insertHtml() {
   "<p>Wins: " + wins + "</p>" +
   "<p>Losses: " + losses + "</p>" +
   "<p>Max Guesses: " + maxGuesses + "</p>";
-  document.querySelector("#win").innerHTML = html;
+  $("#win").html(html);
 
 }
 
